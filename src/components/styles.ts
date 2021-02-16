8ce2eeff75854bba1518a448057acd9713c5a29e
Dustin Toff <me@dustintoff.com>
2021-02-15 23:11:23 -0500

    Refactor for simpler version

--- src/components/styles.ts
+++
@@ -1,7 +0,0 @@
-export const Colors = {
-    // Sky: "#140051", // "black"
-    // Sky: "#1b2240",
-    Sky: "#07213c",
-    // Star: "#fbb931",
-    Star: "#f9e2b2",
-};
--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,13 @@ module.exports = {
     devServer: {
         contentBase: "./dist",
     },
+    output: {
+      publicPath: '/',
+    },
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js"],
+        extensions: [".ts", ".js"],
     },
 
     plugins: [
@@ -49,6 +52,18 @@ module.exports = {
                 ],
             },
             {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images/",
+                        },
+                    },
+                ],
+            },
+            {
                 test: /\.ttf$/,
                 use: [
                     {
@@ -59,16 +74,7 @@ module.exports = {
                         },
                     },
                 ],
-            },
+            }
         ],
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    // externals: {
-    //     "react": "React",
-    //     "react-dom": "ReactDOM"
-    // }
 };
