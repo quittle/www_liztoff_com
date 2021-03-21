c877d18f43be18e1f40a9eec2040234ee7efaed9
Dustin Toff <me@dustintoff.com>
2021-03-21 19:47:15 -0400

    Migrate all images from webp

--- config/webpack-utils.js
+++ config/webpack-utils.js
@@ -1,3 +1,3 @@
-exports.IMAGE_REGEX = /\.(png|svg|jpg|jpeg|gif|webp)$/i;
+exports.IMAGE_REGEX = /\.(png|svg|jpg|jpeg|gif)$/i;
 exports.FONT_REGEX = /\.(eot|ttf|woff|woff2)$/i;
 exports.SASS_REGEX = /\.s[ac]ss$/i;
