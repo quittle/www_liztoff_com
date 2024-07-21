b0fe4141cb333de29bf379f6f360001cdcc9c38d
Dustin Toff <me@dustintoff.com>
2024-07-21 10:06:31 -0400

    Upgrade dependencies

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
+const { createDefaultPreset } = require("ts-jest");
 
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    globals: {
-        "ts-jest": {
+    transform: {
+        ...createDefaultPreset({
             tsconfig: path.resolve(__dirname, "tsconfig.test.json"),
-        },
+        }).transform,
     },
     rootDir: path.resolve(__dirname, ".."),
 };
